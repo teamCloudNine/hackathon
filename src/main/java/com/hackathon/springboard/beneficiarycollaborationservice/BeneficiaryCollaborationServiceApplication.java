a040fb5730e73ca7b08940089b98bbfdb5bab352
Poss111 <rixxroid@gmail.com>
2023-09-21 22:42:23 -0500

    Adding in default server so remote swagger works

--- src/main/java/com/hackathon/springboard/beneficiarycollaborationservice/BeneficiaryCollaborationServiceApplication.java
+++ src/main/java/com/hackathon/springboard/beneficiarycollaborationservice/BeneficiaryCollaborationServiceApplication.java
@@ -1,5 +1,7 @@
 package com.hackathon.springboard.beneficiarycollaborationservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +11,11 @@ import org.springframework.context.annotation.ComponentScan;
 		basePackages = {
 				"org.openapitools.configuration",
 				"com.hackathon.springboard.beneficiarycollaborationservice"
+		}
+)
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
 		}
 )
 public class BeneficiaryCollaborationServiceApplication {
