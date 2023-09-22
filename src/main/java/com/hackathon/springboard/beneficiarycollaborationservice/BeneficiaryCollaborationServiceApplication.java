016f81f671efd2356fe12756af54d0e7fe99b9ea
Poss111 <rixxroid@gmail.com>
2023-09-21 21:03:00 -0500

    resolves #1 - Adding in swagger configuration

--- src/main/java/com/hackathon/springboard/beneficiarycollaborationservice/BeneficiaryCollaborationServiceApplication.java
+++ src/main/java/com/hackathon/springboard/beneficiarycollaborationservice/BeneficiaryCollaborationServiceApplication.java
@@ -2,8 +2,15 @@ package com.hackathon.springboard.beneficiarycollaborationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(
+		basePackages = {
+				"org.openapitools.configuration",
+				"com.hackathon.springboard.beneficiarycollaborationservice"
+		}
+)
 public class BeneficiaryCollaborationServiceApplication {
 
 	public static void main(String[] args) {
